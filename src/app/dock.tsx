6bd4b80975b7b963807b55388046493fb54f9b1a
sohanemon <m.sohanemon@gmail.com>
2023-05-07 14:52:43 +0600

    refactored

--- src/app/dock.tsx
+++ src/app/dock.tsx
@@ -1,10 +1,14 @@
 'use client';
 
-import { motion, useMotionValue, useTransform } from 'framer-motion';
+import {
+  MotionValue,
+  motion,
+  useMotionValue,
+  useTransform,
+} from 'framer-motion';
 
 export default function Dock() {
-  const mouseX = useMotionValue(0);
-  const width = useTransform(mouseX, [0, 300], [40, 100]);
+  const mouseX = useMotionValue(Infinity);
   //   #note when mouseX = 0 then width = 40
   // and when mouseX = 300 then width = 100
   // #note don't forget to add React when you catch the type by hovering for typescript
@@ -13,18 +17,24 @@ export default function Dock() {
   }
 
   return (
-    <div
+    <motion.div
       onMouseMove={(e) => handleMouseMove(e)}
+      onMouseLeave={(e) => mouseX.set(Infinity)}
       //   #note items-end is !important here
       className='absolute bottom-0 flex items-end h-16 gap-5 px-4 py-3 mx-auto my-5 -translate-x-1/2 bg-neutral-600 w-max rounded-3xl left-1/2'
     >
       {[...Array(5).fill(0)].map((el, idx) => (
-        <motion.div
-          style={{ width }}
-          className='w-10 bg-gray-300 rounded-full cursor-pointer aspect-square'
-          key={idx}
-        ></motion.div>
+        <Icon key={idx} mouseX={mouseX} />
       ))}
-    </div>
+    </motion.div>
+  );
+}
+function Icon({ mouseX }: { mouseX: MotionValue<number> }) {
+  const width = useTransform(mouseX, [0, 300, 500], [40, 60, 40]);
+  return (
+    <motion.div
+      style={{ width }}
+      className='w-10 bg-gray-300 rounded-full cursor-pointer aspect-square'
+    ></motion.div>
   );
 }
